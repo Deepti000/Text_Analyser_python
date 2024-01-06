ec45fedac9c776a049843a8f7554974defe5e6f4
Deepti000 <65061604+Deepti000@users.noreply.github.com>
2024-01-06 23:34:45 +0530

    Update and rename Analyse.ipynb to Analyse.py

---
+++ Analyse.py
@@ -0,0 +1,38 @@
+#Press Shift+Enter to run the code
+givenstring="Lorem ipsum dolor! diam amet, consetetur Lorem magna. sed diam nonumy eirmod tempor. diam et labore? et diam magna. et diam amet."
+
+#Press Shift+Enter to run the code
+import string
+class TextAnalyzer(object):
+    
+    def __init__ (self, text):
+        # remove punctuation
+        ftext = text.replace('.','').replace('!','').replace('?','').replace(',','')
+        # make text lowercase
+        self.fmtText = ftext.lower()
+        
+    def freqAll(self):        
+        # split text into words
+        word = self.fmtText.split()
+        # Create dictionary
+        dic = {}
+        for i in word:
+            if not(i in dic):
+                dic[i] = 1
+            else:
+                dic[i] += 1
+        return dic
+           
+    def freqOf(self,word):
+        # get frequency map
+        dic = self.freqAll()
+        if( word in dic):
+            return dic[word]
+        else:
+            return 0
+
+
+obj1 = TextAnalyzer(givenstring)
+print("Formatted Text:", obj1.fmtText)
+obj1.freqAll()
+obj1.freqOf("lorem")
